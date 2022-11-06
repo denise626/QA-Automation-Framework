3e25d737278cc1472b4c06c3a6420aa60c2d41a9
denise626 <denise626@hotmail.com>
2022-11-06 16:18:36 -0500

    initial commit of Homework23
    Used Page Factory, @FindBy, and fluents

---
+++ src/test/java/POM/pages/HomePage.java
@@ -0,0 +1,21 @@
+package POM.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage {
+    @FindBy(css = "img.avatar")
+        WebElement avatarLocator;
+
+
+    public HomePage(WebDriver givenDriver) {
+        super(givenDriver);
+    }
+    public boolean isUserAvatarDisplayed() {
+        //return driver.findElement(avatarLocator).isDisplayed();
+        avatarLocator.isDisplayed();
+        return this.isUserAvatarDisplayed();
+    }
+}
+
---
+++ src/test/java/POM/pages/LoginPage.java
@@ -0,0 +1,37 @@
+package POM.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+
+    private @FindBy(css = "[type='email']")
+    WebElement emailField;
+
+    private @FindBy(css = "[type='password']")
+    WebElement passwordField;
+    private @FindBy(css = "[type='submit']")
+    WebElement submitButtonLocator;
+
+    public LoginPage(WebDriver givenDriver) {
+        super(givenDriver);
+    }
+    public LoginPage clickSubmitBtn() {
+        submitButtonLocator.click();
+        return this;
+    }
+    public LoginPage provideEmail(String email) {
+        emailField.sendKeys(email);
+        return this;
+    }
+    public LoginPage providePassword(String password) {
+        passwordField.sendKeys(password);
+        return this;
+    }
+    public void login() {
+        provideEmail("denise626@hotmail.com").providePassword("te$t$tudent").clickSubmitBtn();
+    }
+}
+
